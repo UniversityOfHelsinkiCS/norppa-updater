227a08159a27c6af1a6279520c1d6cadc23f26e1
Veikkosuhonen <veikko.suhonen@helsinki.fi>
2023-04-12 15:13:19 +0300

    Wait and retry when importer asks to do so

--- src/updater/importerClient.js
+++ src/updater/importerClient.js
@@ -13,6 +13,14 @@ const fetchData = async (url, params) => {
   const { data } = await importerClient.get(`palaute/updater/${url}`, {
     params,
   })
+
+  if (data.waitAndRetry) {
+    // importer is working to prepare data. Wait a bit and try again
+    const waitTime = data.waitTime ?? 1000
+    await new Promise((resolve) => { setTimeout(resolve, waitTime) })
+    return fetchData(url, params) 
+  }
+
   return data
 }
 
