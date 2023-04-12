7b0bbee1f2c5f05c00d3c7a622e4333fdb920260
Veikkosuhonen <veikko.suhonen@helsinki.fi>
2023-04-12 15:20:38 +0300

    Log message when waiting

--- src/updater/importerClient.js
+++ src/updater/importerClient.js
@@ -1,6 +1,7 @@
 const axios = require('axios')
 
 const { IMPORTER_API_URL, API_TOKEN } = require('../util/config')
+const logger = require('../util/logger')
 
 const importerClient = axios.create({
   baseURL: IMPORTER_API_URL,
@@ -17,8 +18,9 @@ const fetchData = async (url, params) => {
   if (data.waitAndRetry) {
     // importer is working to prepare data. Wait a bit and try again
     const waitTime = data.waitTime ?? 1000
+    logger.info(`[UPDATER] Importer told me to wait ${waitTime}ms before retrying`)
     await new Promise((resolve) => { setTimeout(resolve, waitTime) })
-    return fetchData(url, params) 
+    return fetchData(url, params)
   }
 
   return data
