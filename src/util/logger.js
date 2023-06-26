e6ebb0221b937394ce517b24f3625afcd5c553ec
Miko Keskimäki <miko.keskimaki@helsinki.fi>
2023-06-26 13:00:08 +0300

    Updater logs to graylog

--- src/util/logger.js
+++ src/util/logger.js
@@ -1,4 +1,8 @@
+const os = require('os')
+
 const winston = require('winston')
+const { WinstonGelfTransporter } = require('winston-gelf-transporter')
+
 const { inProduction } = require('./config')
 
 const { combine, timestamp, printf, splat } = winston.format
@@ -42,6 +46,20 @@ if (inProduction) {
   )
 
   transports.push(new winston.transports.Console({ format: prodFormat }))
+
+  transports.push(
+    new WinstonGelfTransporter({
+      handleExceptions: true,
+      host: 'svm-116.cs.helsinki.fi',
+      port: 9503,
+      protocol: 'udp',
+      hostName: os.hostname(),
+      additional: {
+        app: 'norppa-updater',
+        environment: 'production'
+      }
+    })
+  )
 }
 
 const logger = winston.createLogger({ transports })
