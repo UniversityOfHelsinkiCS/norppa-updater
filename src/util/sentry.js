8df1d6f926def8519944ac5e7b797e07e2caaf94
akskokki <akseli.kokki@gmail.com>
2026-05-12 12:15:19 +0300

    config: update sentry config

--- src/util/sentry.js
+++ src/util/sentry.js
@@ -3,16 +3,13 @@ const Tracing = require('@sentry/tracing') // eslint-disable-line
 // Sentry docs Note: You MUST import the package for tracing to work
 const { inProduction, inStaging, GIT_SHA } = require('./config')
 
-const initializeSentry = (router) => {
+const initializeSentry = router => {
   if (!inProduction || inStaging) return
 
   Sentry.init({
-    dsn: 'https://c2bfd74ae83d84b110d89d5ca00e560f@toska.cs.helsinki.fi/20',
+    dsn: 'https://9f5da128bdfdb195ac88ef3c6fe66701@toska.it.helsinki.fi/30',
     release: GIT_SHA,
-    integrations: [
-      new Sentry.Integrations.Http({ tracing: true }),
-      new Tracing.Integrations.Express({ router }),
-    ],
+    integrations: [new Sentry.Integrations.Http({ tracing: true }), new Tracing.Integrations.Express({ router })],
     tracesSampleRate: 1.0,
   })
 }
